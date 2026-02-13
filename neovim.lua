d3118aa2da8564d354a146f29806813f522e2042
Marco Gallegos <marco.gallegos@outlook.com>
2026-02-12 18:29:49 -0600

    Update Neovim and Warp terminal colors to Catppuccin Macchiato palette

--- neovim.lua
+++ neovim.lua
@@ -7,24 +7,24 @@ return {
             disable_italics = false,
             colors = {
                 -- Monotone shades (base00-base07)
-                base00 = "#232829", -- Default background
-                base01 = "#788587", -- Lighter background (status bars)
-                base02 = "#232829", -- Selection background
-                base03 = "#788587", -- Comments, invisibles
-                base04 = "#d6dcdb", -- Dark foreground
-                base05 = "#dcd6d6", -- Default foreground
-                base06 = "#dcd6d6", -- Light foreground
-                base07 = "#d6dcdb", -- Light background
+                base00 = "#24273a", -- Default background
+                base01 = "#5b6078", -- Lighter background (status bars)
+                base02 = "#24273a", -- Selection background
+                base03 = "#5b6078", -- Comments, invisibles
+                base04 = "#b8c0e0", -- Dark foreground
+                base05 = "#cad3f5", -- Default foreground
+                base06 = "#cad3f5", -- Light foreground
+                base07 = "#b8c0e0", -- Light background
 
                 -- Accent colors (base08-base0F)
-                base08 = "#b39a98", -- Variables, errors, red
-                base09 = "#b39898", -- Integers, constants, orange
-                base0A = "#93b3a6", -- Classes, types, yellow
-                base0B = "#98b3a3", -- Strings, green
-                base0C = "#98b3b3", -- Support, regex, cyan
-                base0D = "#85abbc", -- Functions, keywords, blue
-                base0E = "#bea7b0", -- Keywords, storage, magenta
-                base0F = "#c8dad3", -- Deprecated, brown/yellow
+                base08 = "#ed8796", -- Variables, errors, red
+                base09 = "#ee99a0", -- Integers, constants, orange
+                base0A = "#eed49f", -- Classes, types, yellow
+                base0B = "#a6da95", -- Strings, green
+                base0C = "#8bd5ca", -- Support, regex, cyan
+                base0D = "#8aadf4", -- Functions, keywords, blue
+                base0E = "#c6a0f6", -- Keywords, storage, magenta
+                base0F = "#f5a97f", -- Deprecated, brown/yellow
             },
         },
         config = function(_, opts)
